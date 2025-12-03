b4ad8a259f327cff249c293780f03266c5aed982
hieunhangia <hieuvippro531@gmail.com>
2025-12-03 17:44:15 +0700

    update using attribute in code-first ef core and add basic ai chat entity

--- Core/Entities/Users/Customer.cs
+++ Core/Entities/Users/Customer.cs
@@ -1,7 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Core.Entities.Ai;
+using Microsoft.EntityFrameworkCore;
+
 namespace Core.Entities.Users;
 
 public class Customer : User
 {
-    public int DefaultShippingInformationId { get; set; }
+    public int? DefaultShippingInformationId { get; set; }
+    [ForeignKey(nameof(DefaultShippingInformationId))]
+    [DeleteBehavior(DeleteBehavior.NoAction)]
+    public ShippingInformation? DefaultShippingInformation { get; set; }
+    
+    [InverseProperty(nameof(ShippingInformation.Customer))]
     public ICollection<ShippingInformation>? ShippingInformations { get; set; }
+    
+    [InverseProperty(nameof(AiConversation.Customer))]
+    public ICollection<AiConversation>? Conversations { get; set; }
 }
