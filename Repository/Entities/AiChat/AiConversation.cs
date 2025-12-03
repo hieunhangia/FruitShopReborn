5b6c44bdd934fb5ef19a1f4a921a9e39d1c69c94
hieunhangia <hieuvippro531@gmail.com>
2025-12-03 17:58:44 +0700

    rename ai chat entity folder

--- Repository/Entities/AiChat/AiConversation.cs
+++ Repository/Entities/AiChat/AiConversation.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Repository.Entities.Users;
 
-namespace Repository.Entities.Ai;
+namespace Repository.Entities.AiChat;
 
 public class AiConversation
 {
